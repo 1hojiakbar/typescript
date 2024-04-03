ad45c1f24a29b6de49e755b3632fbb6240a420f7
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-03 20:59:04 +0500

    null and undefined in TS

--- #2data-types/index.js
+++ #2data-types/index.js
@@ -14,6 +14,20 @@
 // let nums: number = 123; // => success
 // ===================
 // BigInt
-let big = 123n; // bigInt ES2020-version"da chiqani uchun ES2020"dan oldingi versiyalar buni qo'llab quvatlamaydi
+// let big: bigint = 123n; // bigInt ES2020-version"da chiqani uchun ES2020"dan oldingi versiyalar buni qo'llab quvatlamaydi
 // big = 123 // error
 // big = 123n // success
+// ===================
+// null & undefined
+// let nums: number = 123;
+// nums = undefined;
+// Agar biz "tsconfig.json" file ichidan strict malumotini comment"ga olib qo'ysak
+// biz hohlagan o'zgaruvchimizga null va undefined qiymatini biriktirib ketishimiz mumkin bo'ladi aks holda bizga error beradi
+// console.log(nums);
+// Lekin agar keyinchalik strict malumotiga ehtiyoj sezsak uni yoqib o'rniga
+// strictNullChecks comment"dan chiqarib qo'yishimiz bizga huddi usha imkonyat ya'ni
+// null va undefined"ni hohlagan o'zgaruvchimizga biriktirish imkonini beradi.
+// Example:
+// let und = null;
+// und = undefined;
+// console.log(und); // undefined
