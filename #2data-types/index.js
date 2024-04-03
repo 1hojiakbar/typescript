15e0e2e37f3e3e769806e825c1bd6ca80d8166c3
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-03 19:42:17 +0500

    data types in TS

--- #2data-types/index.js
+++ #2data-types/index.js
@@ -1,4 +1,6 @@
 "use strict";
 // *data types: string, number, bigInt, symbol, boolean null undefined
 let title = "data types";
+// title = 777; // malumotlar turlarini boshlang'ish qiymat turini keyinchalik boshqa turga mansub qiymatga belgilab
+// ketish mumkin emas
 console.log(title);
--- #2data-types/index.ts
+++ #2data-types/index.ts
@@ -1,4 +1,8 @@
 // *data types: string, number, bigInt, symbol, boolean null undefined
 
 let title = "data types";
+
+// title = 777; // malumotlar turlarini boshlang'ish qiymat turini keyinchalik boshqa turga mansub qiymatga belgilab
+// ketish mumkin emas
+
 console.log(title);
