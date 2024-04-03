9ea219dd2e86dd04cd58e7c6782224d8802ce0c4
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-03 20:26:12 +0500

    string and number data types in TS

--- #2data-types/index.js
+++ #2data-types/index.js
@@ -5,5 +5,10 @@
 // malumotlar turlarini boshlang'ish qiymat turini keyinchalik boshqa turga mansub qiymatga belgilab ketish mumkin emas
 // console.log(title);
 // ===================
+// String
+let str = "abc"; // success
+let strs = 123; // error
+// ===================
+// Number
 let num = "abc"; // => error
 let nums = 123; // => success
