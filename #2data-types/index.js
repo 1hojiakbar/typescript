22a8fcff2ded258054cfa7e3c2c596686668352b
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-03 20:25:45 +0500

    string and number data type in TS

--- #2data-types/index.js
+++ #2data-types/index.js
@@ -1,6 +1,9 @@
 "use strict";
 // *data types: string, number, bigInt, symbol, boolean null undefined
-let title = "data types";
+// let title = "data types";
 // title = 777; // => error
 // malumotlar turlarini boshlang'ish qiymat turini keyinchalik boshqa turga mansub qiymatga belgilab ketish mumkin emas
-console.log(title);
+// console.log(title);
+// ===================
+let num = "abc"; // => error
+let nums = 123; // => success
--- #2data-types/index.ts
+++ #2data-types/index.ts
@@ -1,8 +1,22 @@
 // *data types: string, number, bigInt, symbol, boolean null undefined
 
-let title = "data types";
+// let title = "data types";
 
 // title = 777; // => error
 // malumotlar turlarini boshlang'ish qiymat turini keyinchalik boshqa turga mansub qiymatga belgilab ketish mumkin emas
 
-console.log(title);
+// console.log(title);
+
+// ===================
+
+// String
+let str: string = "abc"; // success
+
+let strs: string = 123; // error
+
+// ===================
+
+// Number
+let num: number = "abc"; // => error
+
+let nums: number = 123; // => success
