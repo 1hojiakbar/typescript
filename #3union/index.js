07d69bf06510a8843b90e35406b681fa414d64a9
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-04 23:38:56 +0500

    union

--- #3union/index.js
+++ #3union/index.js
@@ -1,2 +1,5 @@
 "use strict";
 // Union
+let str1 = "123"; // error
+let str2 = 123; // success
+console.log(str2); // 123
--- #3union/index.ts
+++ #3union/index.ts
@@ -1,1 +1,7 @@
 // Union
+
+let str1: number = "123"; // error
+
+let str2: string | number = 123; // success
+
+console.log(str2); // 123
