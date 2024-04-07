651ad53a5356c17ce24db27cf0b049696d02b66b
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-08 00:32:34 +0500

    interface in typescript

--- #4type-interface/index.ts
+++ #4type-interface/index.ts
@@ -1,24 +1,33 @@
 // type | interface
 
 // type keyword
-type Lang = "uzbek" | "english" | "rus";
-type DataType = { id?: number; value: Lang }[];
+// type Lang = "uzbek" | "english" | "rus";
+// type DataType = { id?: number; value: Lang }[];
 
 // #discription: Biz TypeScript-dagi "type" kalit so'z orqali ma'lumotlar shaklini aniqlash uchun foydalanishimiz mumkin.
 
-let language: Lang = "rus";
+// let language: Lang = "rus";
 
-const data: DataType = [
-  { value: "uzbek" },
-  { id: 2, value: "english" },
-  { id: 3, value: "rus" },
-];
+// const data: DataType = [
+//   { value: "uzbek" },
+//   { id: 2, value: "english" },
+//   { id: 3, value: "rus" },
+// ];
 
-data.map((lang) => {
-  language = lang.value;
-});
+// data.map((lang) => {
+//   language = lang.value;
+// });
 
 // =========================
 
 // type"ga function biriktirish
-type Func = (title?: string, age?: number) => void;
+// type Func = (title?: string, age?: number) => void;
+
+// =========================
+
+interface Data {
+  id?: number;
+  value: string;
+}
+
+// #discription: TypeScript-da interfeys ob'ekt rioya qilishi kerak bo'lgan shartnomani belgilaydi.
