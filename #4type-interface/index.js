a19d96acd31396acb3a3ed5b617c9f184b5105c7
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-08 06:51:53 +0500

    "&" in type

--- #4type-interface/index.js
+++ #4type-interface/index.js
@@ -1,5 +1,58 @@
 "use strict";
 // type | interface
+// type keyword
+// type Lang = "uzbek" | "english" | "rus";
+// type DataType = { id?: number; value: Lang }[];
+// #discription: Biz TypeScript-dagi "type" kalit so'z orqali ma'lumotlar shaklini aniqlash uchun foydalanishimiz mumkin.
+// let language: Lang = "rus";
+// const data: DataType = [
+//   { value: "uzbek" },
+//   { id: 2, value: "english" },
+//   { id: 3, value: "rus" },
+// ];
+// data.map((lang) => {
+//   language = lang.value;
+// });
+// =========================
+// type"ga function biriktirish
+// type Func = (title?: string, age?: number) => void;
+// =========================
+// interface Data {
+//   id?: number;
+//   value: string;
+// }
+// #discription: TypeScript-da interfeys ob'ekt rioya qilishi kerak bo'lgan shartnomani belgilaydi.
+// =========================
+// Example:
+// type Lang = "uzbek" | "english" | "rus";
+// interface Obj {
+//   id?: number;
+//   value: Lang;
+// }
+// type DataType = Obj[];
+// const data: DataType = [
+//   { value: "uzbek" },
+//   { id: 2, value: "english" },
+//   { id: 3, value: "rus" },
+// ];
+// interface"lar bizga array ichidagi object"larni soddalashtirib yozish uchun qo'l keladi.
+// =========================
+// interface vs type
+// type Obj = {
+//   age: number;
+// };
+// type Obj = {}; // error: Duplicate identifier 'Obj'
+// biz agar type kalit so'zi bilan ikkta bir xil malumot turi yaratsak bizga error kelib chiqadi
+// ============
+// interface vs type
+// interface Data {
+//   name: string;
+// }
+// interface Data {
+//   name: string;
+// }
 // bu holatda bizga error bermaydi lekin bu biz bir xil nomga ega malumot yaratib ketishimiz mumkin degani emas chunki
 // biz agar ikkta bir xil nomga ega malumot"ni "interface" bilan elon qilib ketsak bizga "overwrite" holati yuzaga keladi.
 // shuning uchun biz bunday holatda interface o'rniga type ishlatishimiz maqsadga muvofiq bo'ladi.
+// =========================
+// 13:46
--- #4type-interface/index.ts
+++ #4type-interface/index.ts
@@ -68,14 +68,31 @@
 
 // interface vs type
 
-interface Data {
-  name: string;
-}
+// interface Data {
+//   name: string;
+// }
 
-interface Data {
-  name: string;
-}
+// interface Data {
+//   name: string;
+// }
 
 // bu holatda bizga error bermaydi lekin bu biz bir xil nomga ega malumot yaratib ketishimiz mumkin degani emas chunki
 // biz agar ikkta bir xil nomga ega malumot"ni "interface" bilan elon qilib ketsak bizga "overwrite" holati yuzaga keladi.
 // shuning uchun biz bunday holatda interface o'rniga type ishlatishimiz maqsadga muvofiq bo'ladi.
+
+// =========================
+
+type Base = {
+  id: number;
+  key: string;
+};
+
+type ListBase = {
+  found: number;
+  title: string;
+};
+
+type TupleBase = Base & {
+  age: number;
+  title: string;
+};
