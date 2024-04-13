f2c26654a8da9a59a37d1eaf30abde33f0f1e8b7
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-13 17:48:26 +0500

    array in ts

--- #5 array/index.ts
+++ #5 array/index.ts
@@ -4,35 +4,38 @@
 // that's a simple empty array
 
 // let ar: number[] = [1, 2, 3, 4, 5];
+// let ar: Array<number> = [12,3,4];
 // this array only accepts the number value
 
 // let arrStr: string[] = ["abc", "cba"];
+// let arr: Array<string> = ["abc", "cba"];
 // this array only accepts the string value
 
 // ===========================
 
 // union with array
 // let arr: (number | string)[] = [1, 2, 3, "abc", "cba"];
+// let ar: Array<string | number> = [123, "abc"];
 // this array only accepts the string and number value
 
 // let ar: (number | string)[] = [1, 2, 3, 4, 5, 6, "abc", "werd"];
 
 // ===========================
 
-interface User {
-  id: number;
-  name: string;
-  age: number;
-}
+// interface User {
+//   id: number;
+//   name: string;
+//   age: number;
+// }
 
-type Data = User[];
+// type Data = User[];
 
-let data: Data = [
-  { id: 1, name: "user name", age: 24 },
-  { id: 2, name: "user name", age: 24 },
-  { id: 3, name: "user name", age: 24 },
-  { id: 4, name: "user name", age: 24 },
-  { id: 5, name: "user name", age: 24 },
-];
+// let data: Data = [
+//   { id: 1, name: "user name", age: 24 },
+//   { id: 2, name: "user name", age: 24 },
+//   { id: 3, name: "user name", age: 24 },
+//   { id: 4, name: "user name", age: 24 },
+//   { id: 5, name: "user name", age: 24 },
+// ];
 
-console.log(data[0].id); // 1
+// console.log(data[0].id); // 1
