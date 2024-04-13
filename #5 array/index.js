6a06dbb89cb5f3474126e1aa1bc81ed76881262b
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-04-13 14:35:00 +0500

    array in ts

--- #5 array/index.js
+++ #5 array/index.js
@@ -4,3 +4,6 @@ let arr = [];
 // that's a simple empty array
 let ar = [1, 2, 3, 4, 5];
 // this array only accepts the number value
+let arrStr = ["abc", "cba"];
+// this array only accepts the string value
+// ===========================
--- #5 array/index.ts
+++ #5 array/index.ts
@@ -5,3 +5,8 @@ let arr: [] = [];
 
 let ar: number[] = [1, 2, 3, 4, 5];
 // this array only accepts the number value
+
+let arrStr: string[] = ["abc", "cba"];
+// this array only accepts the string value
+
+// ===========================
