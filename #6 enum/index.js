ea29936a0e5644f85921a7670d0c4e7c7ada0098
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-11 10:17:12 +0500

    enum

--- #6 enum/index.js
+++ #6 enum/index.js
@@ -2,8 +2,8 @@
 // Enum
 var Enu;
 (function (Enu) {
-    Enu[Enu["uzbek"] = 0] = "uzbek";
-    Enu[Enu["russian"] = 1] = "russian";
-    Enu[Enu["english"] = 2] = "english";
+    Enu[Enu["uzbek"] = 2] = "uzbek";
+    Enu[Enu["russian"] = 4] = "russian";
+    Enu[Enu["english"] = 5] = "english";
 })(Enu || (Enu = {}));
 console.log(Enu);
--- #6 enum/index.ts
+++ #6 enum/index.ts
@@ -1,9 +1,57 @@
 // Enum
 
-enum Enu {
-  uzbek,
-  russian,
-  english,
-}
+// enum
+// enum Enu {
+//   uzbek,
+//   russian,
+//   english,
+// }
 
-console.log(Enu);
+// console.log(Enu);
+
+// ----------------------
+
+// Number enum
+// enum Enu {
+//   uzbek = 10,
+//   russian,
+//   english,
+// }
+
+// console.log(Enu);
+
+// ----------------------
+
+// String enum
+// enum Enu {
+//   uzb = "uzb",
+//   rus = "rus",
+//   eng = "eng",
+// }
+
+// console.log(Enu);
+
+// ----------------------
+
+// Gibrid enum
+// enum Enu {
+//   uzb = 10,
+//   rus = "rus",
+//   eng = 11,
+// }
+
+// console.log(Enu);
+
+// ----------------------
+
+// const enum
+
+// "const" will always delete the compile, if we enable "preserveConstEnums: true" it will prevent the compile from being deleted.
+
+// const enum Enu {
+//   uzbe,
+//   rus,
+//   eng,
+// }
+
+// it cannot be compiled into a js file but can return the result to the screen
