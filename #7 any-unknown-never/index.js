88681a435498b189187c179c234f12a3f300b254
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-11 11:38:32 +0500

    #7 any

---
+++ #7 any-unknown-never/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Any
+let num;
+num = 5; // number type
+num = "Hello"; // string type
+num = true; // boolean type
---
+++ #7 any-unknown-never/index.ts
@@ -0,0 +1,5 @@
+// Any
+let num: any;
+num = 5; // number type
+num = "Hello"; // string type
+num = true; // boolean type
