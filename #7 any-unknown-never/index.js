2e2d86dd863fe33b7cd74f506fc20635726bcef3
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-11 12:01:41 +0500

    #7 lesson any, unknown, never

--- #7 any-unknown-never/index.js
+++ #7 any-unknown-never/index.js
@@ -6,23 +6,21 @@
 // a = true; // boolean type
 // ------------------
 // unknown
-function getMessage(input) {
-    if (typeof input === "string") {
-        console.log(`String input: ${input.toUpperCase()}`);
-    }
-    else if (typeof input === "number") {
-        console.log(`Number input: ${input.toFixed(2)}`);
-    }
-    else {
-        console.log(`Unknown type: ${input}`);
-    }
-}
-getMessage("Hello");
-getMessage(3.1415);
-getMessage(true);
+// function getMessage(input: unknown): void {
+//   if (typeof input === "string") {
+//     console.log(`String input: ${input.toUpperCase()}`);
+//   } else if (typeof input === "number") {
+//     console.log(`Number input: ${input.toFixed(2)}`);
+//   } else {
+//     console.log(`Unknown type: ${input}`);
+//   }
+// }
+// getMessage("Hello");
+// getMessage(3.1415);
+// getMessage(true);
 // ------------------
 // never
-function throwError(message) {
-    throw new Error(message); // Bu funksiya hech qachon tugamaydi, chunki u har doim xato tashlaydi
-}
-let a; // this variable never get the value
+// function throwError(message: string): never {
+//   throw new Error(message); // Bu funksiya hech qachon tugamaydi, chunki u har doim xato tashlaydi
+// }
+// let a: never; // this variable never get the value
--- #7 any-unknown-never/index.ts
+++ #7 any-unknown-never/index.ts
@@ -7,25 +7,25 @@
 // ------------------
 
 // unknown
-function getMessage(input: unknown): void {
-  if (typeof input === "string") {
-    console.log(`String input: ${input.toUpperCase()}`);
-  } else if (typeof input === "number") {
-    console.log(`Number input: ${input.toFixed(2)}`);
-  } else {
-    console.log(`Unknown type: ${input}`);
-  }
-}
+// function getMessage(input: unknown): void {
+//   if (typeof input === "string") {
+//     console.log(`String input: ${input.toUpperCase()}`);
+//   } else if (typeof input === "number") {
+//     console.log(`Number input: ${input.toFixed(2)}`);
+//   } else {
+//     console.log(`Unknown type: ${input}`);
+//   }
+// }
 
-getMessage("Hello");
-getMessage(3.1415);
-getMessage(true);
+// getMessage("Hello");
+// getMessage(3.1415);
+// getMessage(true);
 
 // ------------------
 
 // never
-function throwError(message: string): never {
-  throw new Error(message); // Bu funksiya hech qachon tugamaydi, chunki u har doim xato tashlaydi
-}
+// function throwError(message: string): never {
+//   throw new Error(message); // Bu funksiya hech qachon tugamaydi, chunki u har doim xato tashlaydi
+// }
 
-let a: never; // this variable never get the value
+// let a: never; // this variable never get the value
