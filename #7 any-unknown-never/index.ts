47247fdf788f1efdf4327d495e0506b15577dfd6
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-11 11:59:19 +0500

    #7 unknown

--- #7 any-unknown-never/index.ts
+++ #7 any-unknown-never/index.ts
@@ -1,5 +1,24 @@
 // Any
-let num: any;
-num = 5; // number type
-num = "Hello"; // string type
-num = true; // boolean type
+// let a: any;
+// a = 5; // number type
+// a = "Hello"; // string type
+// a = true; // boolean type
+
+// ------------------
+
+// unknown
+function getMessage(input: unknown): void {
+  if (typeof input === "string") {
+    console.log(`String input: ${input.toUpperCase()}`);
+  } else if (typeof input === "number") {
+    console.log(`Number input: ${input.toFixed(2)}`);
+  } else {
+    console.log(`Unknown type: ${input}`);
+  }
+}
+
+getMessage("Hello");
+getMessage(3.1415);
+getMessage(true);
+
+// ------------------
