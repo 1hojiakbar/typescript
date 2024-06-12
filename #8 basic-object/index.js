c0718808ebf715c4e02b170443e93fb85842f97b
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-12 23:41:31 +0500

    read only into writable

--- #8 basic-object/index.js
+++ #8 basic-object/index.js
@@ -1,17 +1,31 @@
 "use strict";
 // #8 Basic object
-let obj = {
-    id: 1,
-    name: "Eshmat",
-    status: "developer",
-    student: true,
-    // year is optional property
-};
-let obj2 = {
-    id: 1,
-    name: "Eshmat",
-    status: "developer",
-    student: ["hello world"],
-    year: 2024,
-};
+// interface Type {
+//   id: number;
+//   readonly name: string;
+//   status: string;
+//   student: boolean | string[];
+//   year?: number;
+// }
+// let obj: Type = {
+//   id: 1,
+//   name: "Eshmat",
+//   status: "developer",
+//   student: true,
+//   // year is optional property
+// };
+// let obj2: Type = {
+//   id: 1,
+//   name: "Eshmat",
+//   status: "developer",
+//   student: ["hello world"],
+//   year: 2024,
+// };
 // delete obj.name // error: The operand of a 'delete' operator cannot be a read-only property
+// ----------------
+// read only into writable
+let obj1 = { name: "eshmat" };
+let obj2 = obj1;
+obj2.name = "toshmat";
+console.log(obj1.name); // toshmat
+console.log(obj2.name); // toshmat
--- #8 basic-object/index.ts
+++ #8 basic-object/index.ts
@@ -1,27 +1,39 @@
 // #8 Basic object
 
-interface Type {
-  id: number;
-  readonly name: string;
-  status: string;
-  student: boolean | string[];
-  year?: number;
-}
+// interface Type {
+//   id: number;
+//   readonly name: string;
+//   status: string;
+//   student: boolean | string[];
+//   year?: number;
+// }
 
-let obj: Type = {
-  id: 1,
-  name: "Eshmat",
-  status: "developer",
-  student: true,
-  // year is optional property
-};
+// let obj: Type = {
+//   id: 1,
+//   name: "Eshmat",
+//   status: "developer",
+//   student: true,
+//   // year is optional property
+// };
 
-let obj2: Type = {
-  id: 1,
-  name: "Eshmat",
-  status: "developer",
-  student: ["hello world"],
-  year: 2024,
-};
+// let obj2: Type = {
+//   id: 1,
+//   name: "Eshmat",
+//   status: "developer",
+//   student: ["hello world"],
+//   year: 2024,
+// };
 
 // delete obj.name // error: The operand of a 'delete' operator cannot be a read-only property
+
+// ----------------
+
+// read only into writable
+
+let obj1: { readonly name: string } = { name: "eshmat" };
+let obj2: { name: string } = obj1;
+
+obj2.name = "toshmat";
+
+console.log(obj1.name); // toshmat
+console.log(obj2.name); // toshmat
