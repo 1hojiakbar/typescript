83f85121bd488bfdaea4975f71cd61b8b2049d66
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-13 13:28:46 +0500

    call signature

--- #10 function/index.js
+++ #10 function/index.js
@@ -1,10 +1,6 @@
 "use strict";
 // Function in the TS
-const getData = (prop, ...rest) => {
-    console.log(rest);
-    return {
-        sum: 1,
-        list: [1, 2, 3, 4],
-    };
-};
-console.log(getData({ a: 12, list: [1, 2, 3] }, "a", "b"));
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+console.log(add(3, 1)); // 4
+console.log(subtract(4, 3)); // 1
--- #10 function/index.ts
+++ #10 function/index.ts
@@ -1,23 +1,37 @@
 // Function in the TS
 
 // parametr / argument
-type Rest = {
-  sum: number;
-  list: number[];
-};
+// type Rest = {
+//   sum: number;
+//   list: number[];
+// };
 
-type Prop = {
-  a: number;
-  list: number[];
-  b?: number;
-};
+// type Prop = {
+//   a: number;
+//   list: number[];
+//   b?: number;
+// };
 
-const getData = (prop: Prop, ...rest: string[]): Rest => {
-  console.log(rest);
-  return {
-    sum: 1,
-    list: [1, 2, 3, 4],
-  };
-};
+// const getData = (prop: Prop, ...rest: string[]): Rest => {
+//   console.log(rest);
+//   return {
+//     sum: 1,
+//     list: [1, 2, 3, 4],
+//   };
+// };
 
-console.log(getData({ a: 12, list: [1, 2, 3] }, "a", "b"));
+// console.log(getData({ a: 12, list: [1, 2, 3] }, "a", "b"));
+
+// -----------------------------
+
+// Call Signature
+
+interface Calc {
+  (a: number, b: number): number; // Call Signature
+}
+
+const add: Calc = (a, b) => a + b;
+const subtract: Calc = (a, b) => a - b;
+
+console.log(add(3, 1)); // 4
+console.log(subtract(4, 3)); // 1
