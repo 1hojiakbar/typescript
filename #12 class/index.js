cba7c3ae192d9520da812065d16103c88e4670ec
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-20 19:34:29 +0500

    set get in class

--- #12 class/index.js
+++ #12 class/index.js
@@ -1,16 +1,53 @@
 "use strict";
 // Class
-class Test {
-    constructor() {
-        this.title = "abc";
-    }
-}
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Webbrain_name;
+// class Test {
+// title: string;
+// agar siz tsconfig.json file ichidan "strictPropertyInitialization"ga false qiymat biriktirsangiz bu ogohlantirish yoqoladi
+// age!: number; // ! - bizga o'zgaruvchiga qiymat kelishi yoki kelmasligi haqida malumot beradi
+//   constructor() {
+//     this.title = "abc";
+//   }
+// }
+// class Webbrain {
+//   constructor();
+//   constructor(name: string);
+//   constructor(name: number, age: number);
+//   constructor(a?: any, b?: any) {}
+//   update() {
+//     console.log("update function called");
+//   }
+// }
+// let wba = new Webbrain();
+// let wba1 = new Webbrain("webbrain");
+// let wba2 = new Webbrain(1, 1);
+// -----------------------------------
 class Webbrain {
-    constructor(a, b) { }
-    update() {
-        console.log("update function called");
+    constructor() {
+        _Webbrain_name.set(this, "webbrain");
+    }
+    get getName() {
+        return __classPrivateFieldGet(this, _Webbrain_name, "f");
+    }
+    set getName(name) {
+        if (typeof name === "number")
+            __classPrivateFieldSet(this, _Webbrain_name, "no change", "f");
+        else
+            __classPrivateFieldSet(this, _Webbrain_name, name, "f");
     }
 }
+_Webbrain_name = new WeakMap();
 let wba = new Webbrain();
-let wba1 = new Webbrain("webbrain");
-let wba2 = new Webbrain(1, 1);
+wba.getName = 133;
+console.log(wba.getName);
--- #12 class/index.ts
+++ #12 class/index.ts
@@ -1,24 +1,41 @@
 // Class
 
-class Test {
-  title: string;
-  // agar siz tsconfig.json file ichidan "strictPropertyInitialization"ga false qiymat biriktirsangiz bu ogohlantirish yoqoladi
-  age!: number; // ! - bizga o'zgaruvchiga qiymat kelishi yoki kelmasligi haqida malumot beradi
-  constructor() {
-    this.title = "abc";
-  }
-}
+// class Test {
+// title: string;
+// agar siz tsconfig.json file ichidan "strictPropertyInitialization"ga false qiymat biriktirsangiz bu ogohlantirish yoqoladi
+// age!: number; // ! - bizga o'zgaruvchiga qiymat kelishi yoki kelmasligi haqida malumot beradi
+//   constructor() {
+//     this.title = "abc";
+//   }
+// }
+
+// class Webbrain {
+//   constructor();
+//   constructor(name: string);
+//   constructor(name: number, age: number);
+//   constructor(a?: any, b?: any) {}
+//   update() {
+//     console.log("update function called");
+//   }
+// }
+
+// let wba = new Webbrain();
+// let wba1 = new Webbrain("webbrain");
+// let wba2 = new Webbrain(1, 1);
+
+// -----------------------------------
 
 class Webbrain {
-  constructor();
-  constructor(name: string);
-  constructor(name: number, age: number);
-  constructor(a?: any, b?: any) {}
-  update() {
-    console.log("update function called");
+  #name: string = "webbrain";
+  get getName() {
+    return this.#name;
+  }
+  set getName(name: number | string) {
+    if (typeof name === "number") this.#name = "no change";
+    else this.#name = name;
   }
 }
 
 let wba = new Webbrain();
-let wba1 = new Webbrain("webbrain");
-let wba2 = new Webbrain(1, 1);
+wba.getName = 133;
+console.log(wba.getName);
