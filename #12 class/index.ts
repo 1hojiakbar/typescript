45ea3f04815d94943d1ef3e0d83d0fe0680afa12
Rustamvich <xojiakbarisroilov.777@gmail.com>
2024-06-20 20:04:21 +0500

    class with type & interface

--- #12 class/index.ts
+++ #12 class/index.ts
@@ -1,9 +1,9 @@
-// Class
+// // Class
 
 // class Test {
-// title: string;
-// agar siz tsconfig.json file ichidan "strictPropertyInitialization"ga false qiymat biriktirsangiz bu ogohlantirish yoqoladi
-// age!: number; // ! - bizga o'zgaruvchiga qiymat kelishi yoki kelmasligi haqida malumot beradi
+//   title: string;
+//   // agar siz tsconfig.json file ichidan "strictPropertyInitialization"ga false qiymat biriktirsangiz bu ogohlantirish yoqoladi
+//   age!: number; // ! - bizga o'zgaruvchiga qiymat kelishi yoki kelmasligi haqida malumot beradi
 //   constructor() {
 //     this.title = "abc";
 //   }
@@ -25,17 +25,51 @@
 
 // -----------------------------------
 
-class Webbrain {
-  #name: string = "webbrain";
-  get getName() {
-    return this.#name;
+// class Webbrain {
+//   #name: string = "webbrain";
+//   get getName() {
+//     return this.#name;
+//   }
+//   set getName(name: number | string) {
+//     if (typeof name === "number") this.#name = "no change";
+//     else this.#name = name;
+//   }
+// }
+
+// let wba = new Webbrain();
+// wba.getName = 133;
+// console.log(wba.getName);
+
+// -----------------------------------
+
+// class with type & interface
+
+type AType = {
+  name: string;
+  age: number;
+};
+
+class A implements AType {
+  name = "abc";
+  age = 123;
+  constructor(name: string) {
+    this.name = name;
   }
-  set getName(name: number | string) {
-    if (typeof name === "number") this.#name = "no change";
-    else this.#name = name;
+
+  update() {
+    this.age = 321;
   }
 }
 
-let wba = new Webbrain();
-wba.getName = 133;
-console.log(wba.getName);
+interface BType {
+  title: string;
+}
+
+class B extends A implements BType {
+  title = "eshmat";
+}
+
+const first = new B("abcd");
+
+first.update();
+console.log(first.age);
